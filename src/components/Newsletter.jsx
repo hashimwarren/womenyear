5ff78fe26cca691ff769c96c5050bb6489f7dd3b
Hashim Warren <hashimwarren@gmail.com>
2023-03-11 18:01:45 -0500

    correct netlify form

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -42,8 +42,7 @@ export function Newsletter() {
                 notified when tickets go on sale.
               </p>
             </div>
-            <form >
-              <input type="hidden" name="form-name" value="name_of_my_form" />
+            <form name="newsletter" method="post" data-netlify="true">
 
               <h3 className="text-lg font-semibold tracking-tight text-blue-900">
                 Sign up to our newsletter <span aria-hidden="true">&darr;</span>
